378b15149e35365e4532e79c4fabb413e0287100
AbdulSamadKhan <abdulsamad@users.noreply.huggingface.co>
2023-07-28 11:09:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,19 @@ import base64
 demo = gr.Textbox(get_completion(os.environ['HF_TOKENS']))
 
 #Image-to-text endpoint
-def get_completion(inputs, parameters=None, ENDPOINT_URL="http://internal-aws-prod-internal-revproxy-alb-11660607.us-west-1.elb.amazonaws.com/rev-proxy/huggingface/itt"): 
-    headers = {
-      "Authorization": f"Bearer {os.environ['HF_TOKENS']}",
-      "Content-Type": "application/json"
-    }
-    data = { "inputs": inputs }
-    if parameters is not None:
-        data.update({"parameters": parameters})
-    response = requests.request("POST",
-                                ENDPOINT_URL,
-                                headers=headers,
-                                data=json.dumps(data))
-    return json.loads(response.content.decode("utf-8"))
+#def get_completion(inputs, parameters=None, ENDPOINT_URL="http://internal-aws-prod-internal-revproxy-alb-11660607.us-west-1.elb.amazonaws.com/rev-proxy/huggingface/itt"): 
+#    headers = {
+#      "Authorization": f"Bearer {os.environ['HF_TOKENS']}",
+#      "Content-Type": "application/json"
+#    }
+#    data = { "inputs": inputs }
+#    if parameters is not None:
+#        data.update({"parameters": parameters})
+#    response = requests.request("POST",
+#                                ENDPOINT_URL,
+#                                headers=headers,
+#                                data=json.dumps(data))
+#    return json.loads(response.content.decode("utf-8"))
 
 #image_url = "https://free-images.com/sm/9596/dog_animal_greyhound_983023.jpg"
 #demo = gr.Textbox(get_completion(image_url))
