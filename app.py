3fdb0e4c3b2d744af1c64aa621c8536fac4f26df
AbdulSamadKhan <abdulsamad@users.noreply.huggingface.co>
2023-07-28 14:05:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -14,23 +14,7 @@ model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-capt
 
 
 
-
-
-#def greet(name):
-#    return "Hello " + name +os.environ['HF_TOKENS']
-
-
-#demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-
-#demo.launch()
-
-
-#gr.close_all()
-#gr.Textbox(os.environ['HF_TOKENS'])
-
-#Image-to-text endpoint
 def get_completion(raw_image): 
-    #raw_image = Image.open(image).convert('RGB')
 
     text = "a photography of"
     inputs = processor(raw_image, text, return_tensors="pt")
@@ -39,24 +23,11 @@ def get_completion(raw_image):
     return processor.decode(out[0], skip_special_tokens=True)
     
     
-    #    headers = {
-#      "Authorization": f"Bearer {os.environ['HF_TOKENS']}",
-#      "Content-Type": "application/json"
-#    }
-#    data = { "inputs": inputs }
-#    if parameters is not None:
-#        data.update({"parameters": parameters})
-#    response = requests.request("POST",
-#                                ENDPOINT_URL,
-#                                headers=headers,
-#                                data=json.dumps(data))
-#    return json.loads(response.content.decode("utf-8"))
-
-
-
 
 gr.close_all()
+description = "Made by : Abdul Samad"
 demo = gr.Interface(fn=get_completion,
+                    description=description,
                     inputs=[gr.Image(label="Upload image", type="pil")],
                     outputs=[gr.Textbox(label="Caption")],
                     title="Image Captioning with BLIP",
@@ -67,13 +38,5 @@ demo = gr.Interface(fn=get_completion,
                    )
 
 
-
-#demo = gr.Interface(fn=captioner,
- #                   inputs=[gr.Image(label="Upload image", type="pil")],
- #//                   outputs=[gr.Textbox(label="Caption")],
- # //                  title="Image Captioning with BLIP",
- #  //                 description="Caption any image using the BLIP model",
- #   //                allow_flagging="never",
- #    //               examples=["christmas_dog.jpeg", "bird_flight.jpeg", "cow.jpeg"])
 
 demo.launch()
