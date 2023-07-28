46b8ce820e6995e28174aa17b33da326f823aff5
AbdulSamadKhan <abdulsamad@users.noreply.huggingface.co>
2023-07-28 11:34:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ import IPython.display
 from PIL import Image
 import base64 
 
-gr.close_all()
-gr.Textbox(os.environ['HF_TOKENS'])
+print(os.environ['HF_TOKENS'])
+#gr.close_all()
+#gr.Textbox(os.environ['HF_TOKENS'])
 
 #Image-to-text endpoint
 #def get_completion(inputs, parameters=None, ENDPOINT_URL="http://internal-aws-prod-internal-revproxy-alb-11660607.us-west-1.elb.amazonaws.com/rev-proxy/huggingface/itt"): 
