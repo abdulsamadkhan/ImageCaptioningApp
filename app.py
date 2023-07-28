13ad427b99b9336a48976987e71e798afa2ba3e8
AbdulSamadKhan <abdulsamad@users.noreply.huggingface.co>
2023-07-28 13:59:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,10 @@ demo = gr.Interface(fn=get_completion,
                     outputs=[gr.Textbox(label="Caption")],
                     title="Image Captioning with BLIP",
                     description="Caption any image using the BLIP model",
-                    allow_flagging="never")
+                    allow_flagging="never",
+                   examples=["rottweiler_puppy_dog_background_4.jpg", "rottweiler_dog_wedding_dresses.jpg", "cheetah_animal_predator_525169.jpg",
+                            "woman_cheetah_animal_human.jpg","man_stands_near_wild.jpg","calidris_alba_bird_nature.jpg"]
+                   )
 
 
 
