da8bab20dc7a2ae0b8ea60487d7d6d7d6b6bbb4d
AbdulSamadKhan <abdulsamad@users.noreply.huggingface.co>
2023-07-28 14:07:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -25,13 +25,11 @@ def get_completion(raw_image):
     
 
 gr.close_all()
-description = "Made by : Abdul Samad"
 demo = gr.Interface(fn=get_completion,
-                    description=description,
                     inputs=[gr.Image(label="Upload image", type="pil")],
                     outputs=[gr.Textbox(label="Caption")],
                     title="Image Captioning with BLIP",
-                    description="Caption any image using the BLIP model",
+                    description="Caption any image using the BLIP model, Made by Abdul Samad",
                     allow_flagging="never",
                    examples=["rottweiler_puppy_dog_background_4.jpg", "rottweiler_dog_wedding_dresses.jpg", "cheetah_animal_predator_525169.jpg",
                             "woman_cheetah_animal_human.jpg","man_stands_near_wild.jpg","calidris_alba_bird_nature.jpg"]
